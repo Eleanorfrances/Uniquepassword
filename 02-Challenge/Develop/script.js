1804a07bcd5e22da0a2d0a00be1218617d9afb0e
Eleanor Frances <elfrancy2002@gmail.com>
2023-03-11 15:11:54 -0700

    adding characters to draw from and password length

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,14 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
+  // Adding the charaters the server can draaw from
+  var specialChar = "!#$%&'()*+,-_./:;<=?@}{^"
+  var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM"
+
+
+   
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Choosing password length
+var passwordLength = parseInt(prompt("How long would you like your password?"));
+   
