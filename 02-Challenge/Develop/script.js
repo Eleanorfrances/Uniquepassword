cf86c027048d234d3686a108e8acd300ad2f063c
Eleanor Frances <elfrancy2002@gmail.com>
2023-03-12 20:33:32 -0600

    added the final write passord code

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -10,13 +10,44 @@ function writePassword() {
 
   // Adding the charaters the server can draaw from
   var specialChar = "!#$%&'()*+,-_./:;<=?@}{^"
-  var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM"  
+  var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM" 
+ 
+  // Choosing password length
+var passwordLength = parseInt(prompt("How long would you like your password?"));
 
+if (isNaN(passwordLength), passwordLength < 1 , passwordLength > 127) {
+  alert ("Value must be between 1 - 128");
+  return "";
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword("#password") {
+//booleans to choose special characters and upper case letters
+var specialChar = confirm("Would you like special characters in your password?");
+var upperCase = confirm("Would you like to use upper case letters in your password?");
 
-// Choosing password length
-var passwordLength = parseInt(prompt("How long would you like your password?"));
-});
+let characters = "";
+
+if (upperCase) {
+  writePassword += upperCase;
+}
+
+if (specialChar) {
+  charSet += specialChar;
+}
+
+//making the password
+let writePassword = "";
+for (let i = 0; i < passwordLength; i++) {
+  writepassword += characters.charAt(math.floor(math.random()* characters.length));
+}
+
+return password;
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword()) {
+var password = writePassword } 
+}
+
+
