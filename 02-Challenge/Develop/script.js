21ba302615731e3fc9123d7649a275ef21840b23
Eleanor Frances <elfrancy2002@gmail.com>
2023-06-24 21:29:27 -0600

    fixed my naming and my event listener

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function generatePassword() {
+  var generatedPassword = "";
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  
 
   // Adding the charaters the server can draaw from
   var specialChar = "!#$%&'()*+,-_./:;<=?@}{^"
@@ -15,39 +15,39 @@ function writePassword() {
   // Choosing password length
 var passwordLength = parseInt(prompt("How long would you like your password?"));
 
-if (isNaN(passwordLength), passwordLength < 1 , passwordLength > 127) {
+if (isNaN(passwordLength)|| passwordLength < 1 || passwordLength > 127) {
   alert ("Value must be between 1 - 128");
   return "";
 }
 
 //booleans to choose special characters and upper case letters
-var specialChar = confirm("Would you like special characters in your password?");
-var upperCase = confirm("Would you like to use upper case letters in your password?");
+var includeSpecialChar = confirm("Would you like special characters in your password?");
+var includeUpperCase = confirm("Would you like to use upper case letters in your password?");
 
 let characters = "";
 
-if (upperCase) {
-  writePassword += upperCase;
+if (includeUpperCase) {
+  generatedPassword += upperCase;
 }
 
-if (specialChar) {
-  charSet += specialChar;
+if (includeSpecialChar) {
+  generatedPassword += specialChar;
 }
 
 //making the password
 let writePassword = "";
 for (let i = 0; i < passwordLength; i++) {
-  writepassword += characters.charAt(math.floor(math.random()* characters.length));
+  writepassword += characters.charAt(Math.floor(Math.random()* generatedPassword.length));
 }
 
-return password;
+passwordText.value = password;
 }
 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword()) {
-var password = writePassword } 
-}
+
+generateBtn.addEventListener('click', generatePassword); 
+
 
 
